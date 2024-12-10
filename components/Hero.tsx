0e4b6d3189b06c56ef663f0c16f582b89c9d8539
Mach Deng Mach <freedom.hotel09@gmail.com>
2024-12-10 16:23:31 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import star8 from "@/public/Star 8.svg";
 import vector4 from "@/public/Vector 4.svg";
 import divider from "@/public/Rectangle 68.svg";
-import garden1 from "@/public/garden1.jpg";
+import image from "@/public/garden1.jpg";
 import DateTimePicker from "./DateTimePicker";
 
 const Hero = () => {
