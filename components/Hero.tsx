b459a50db5cbd506876549b42b1df39bceeca3c1
Mach Deng Mach <freedom.hotel09@gmail.com>
2024-12-10 13:34:07 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -99,7 +99,7 @@ const Hero = () => {
       <div className="grid  md:grid-cols-3 lg:grid-cols-3 mb-14 small:mb-16 largesceen:mb-24">
         <div className="md:col-span-2 animate__animated animate__fadeInLeft">
           <h1 className="text-white-100 text-[4.7rem] small:text-[5rem] lg:text-[7.5rem] desktop:text-[10.625rem] heading leading-none mb-6 md:mb-8 lg:mb-14 max-md:text-center">
-            Bank<span className="text-secondary inline-flex">Hotel</span>
+            Garden Resort<span className="text-secondary inline-flex">Hotel</span>
           </h1>
           <p className="uppercase text-xs md:text-sm desktop:text-base largesceen:text-[1.625rem] max-md:text-center text-secondary">rooms // restaurant // congress hall // wine bar</p>
           <div className="border border-white-100 border-opacity-[0.32] block md:hidden w-[60%] mx-auto mt-6"></div>
@@ -108,7 +108,7 @@ const Hero = () => {
         <div className="h-full flex lg:flex-col justify-between md:gap-x-10 lg:gap-x-0 lg:ml-20 2xl:ml-0">
           <div className="flex justify-between">
             <div className="text-white-100 lg:text-sm  2xl:text-[1.375rem] uppercase border border-white-100 border-opacity-[0.32] rounded-full md:px-5   2xl:py-5 2xl:px-10 flex  items-center gap-x-4 2xl:gap-8 leading-none max-lg:hidden">
-              <Image src={star8} alt="star" className="max-2xl:w-[30px] h-auto animi-1" /> since 1973
+              <Image src={star8} alt="star" className="max-2xl:w-[30px] h-auto animi-1" /> since 2010
             </div>
 
             <button className="md:w-[45px] 2xl:w-[4.375rem] max-md:hidden pointer-events-none">
@@ -117,7 +117,7 @@ const Hero = () => {
           </div>
 
           <p className="max-md:text-center max-md:mx-auto w-[85%] md:w-[13.625rem] lg:w-[90%] xl:w-[21.875rem] text-white-100 text-sm lg:text-base largesceen:text-xl leading-[155.556%] max-md:block max-md:mt-6 max-md:leading-[164.286%]">
-            The luxurious hotel in the most beautiful European city with an exclusive restaurant, conference-hall, and art-bar.
+            The luxurious hotel in the most beautiful Bor city with an exclusive restaurant, conference-hall, and art-bar.
           </p>
         </div>
       </div>
@@ -193,13 +193,13 @@ const Hero = () => {
 
       <div className="md:flex justify-between mt-4 lg:mt-12">
         <div className="max-md:hidden">
-          <p className="text-secondary underline uppercase md:text-xs  lg:text-base 2xl:text-lg largesceen:text-[1.625rem] mb-2">+38 032 297 50 20</p>
-          <p className="text-white-100 underline uppercase md:text-xs  lg:text-base 2xl:text-lg largesceen:text-[1.625rem]">8 Lystopadovoho Chynu, Lviv</p>
+          <p className="text-secondary underline uppercase md:text-xs  lg:text-base 2xl:text-lg largesceen:text-[1.625rem] mb-2">+254 702 048209</p>
+          <p className="text-white-100 underline uppercase md:text-xs  lg:text-base 2xl:text-lg largesceen:text-[1.625rem]">Bor, Jonglei State, South Sudan</p>
         </div>
         <div>
           <p className=" md:text-right text-white-100 text-xs uppercase lg:text-base 2xl:text-lg largesceen:text-[1.625rem]">
-            Art & Congress <br />
-            hall
+            Art & Conference<br />
+            Hall
           </p>
         </div>
       </div>
