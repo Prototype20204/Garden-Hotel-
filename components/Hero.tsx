4f925a5c4771dc94d649ca2476a989a33fd7a0f8
Mach Deng Mach <freedom.hotel09@gmail.com>
2024-12-10 15:33:16 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -125,7 +125,7 @@ const Hero = () => {
       <section className="">
         <div className="relative ">
           <Image
-            src={garden1}
+            src={hero}
             alt="hero"
             priority
             placeholder="blur"
