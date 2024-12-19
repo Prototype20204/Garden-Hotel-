272808e4414a6c6c52ab162e5930f20e85939531
Mach Deng Mach <freedom.hotel09@gmail.com>
2024-12-19 16:45:53 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -108,7 +108,7 @@ const Hero = () => {
         <div className="h-full flex lg:flex-col justify-between md:gap-x-10 lg:gap-x-0 lg:ml-20 2xl:ml-0">
           <div className="flex justify-between">
             <div className="text-white-100 lg:text-sm  2xl:text-[1.375rem] uppercase border border-white-100 border-opacity-[0.32] rounded-full md:px-5   2xl:py-5 2xl:px-10 flex  items-center gap-x-4 2xl:gap-8 leading-none max-lg:hidden">
-              <Image src={star8} alt="star" className="max-2xl:w-[30px] h-auto animi-1" /> since 2009
+              <Image src={star8} alt="star" className="max-2xl:w-[30px] h-auto animi-1" /> since 2013
             </div>
 
             <button className="md:w-[45px] 2xl:w-[4.375rem] max-md:hidden pointer-events-none">
@@ -117,8 +117,8 @@ const Hero = () => {
           </div>
 
           <p className="max-md:text-center max-md:mx-auto w-[85%] md:w-[13.625rem] lg:w-[90%] xl:w-[21.875rem] text-white-100 text-sm lg:text-base largesceen:text-xl leading-[155.556%] max-md:block max-md:mt-6 max-md:leading-[164.286%]">
-            The luxurious hotel in the most beautiful African
-            city with an exclusive restaurant, conference-hall, and art-bar.
+            The luxurious hotel in the most beautiful African. L0cated in  Bor , Jonglei  State in South Sudan, 
+           Bor  city with an exclusive restaurant, conference-hall, and art-bar.
           </p>
         </div>
       </div>
